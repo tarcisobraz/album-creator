242043bb8b74ceb154d348471b318f247eaf1a41
Tarciso Braz <tarcisocomp@gmail.com>
2013-10-04 10:14:43 -0300

    add channel.html file

--- index.php
+++ index.php
@@ -10,26 +10,26 @@
 	<body>
 		<div id="fb-root"></div>
 			<script>
-			  window.fbAsyncInit = function() {
+				window.fbAsyncInit = function() {
 				// init the FB JS SDK
 				FB.init({
-				  appId      : 'YOUR_APP_ID',                        // App ID from the app dashboard
-				  channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel file for x-domain comms
+				  appId      : 361402127326796,                        // App ID from the app dashboard
+				  channelUrl : 'http://sheltered-oasis-9211.herokuapp.com/channel.html', // Channel file for x-domain comms
 				  status     : true,                                 // Check Facebook Login status
-				  xfbml      : true                                  // Look for social plugins on the page
+				  xfbml      : false                                  // Look for social plugins on the page
 				});
 
 				// Additional initialization code such as adding Event Listeners goes here
-			  };
+				};
 
-			  // Load the SDK asynchronously
-			  (function(d, s, id){
+				// Load the SDK asynchronously
+				(function(d, s, id){
 				 var js, fjs = d.getElementsByTagName(s)[0];
 				 if (d.getElementById(id)) {return;}
 				 js = d.createElement(s); js.id = id;
 				 js.src = "//connect.facebook.net/en_US/all.js";
 				 fjs.parentNode.insertBefore(js, fjs);
-			   }(document, 'script', 'facebook-jssdk'));
+				}(document, 'script', 'facebook-jssdk'));
 			</script>
 		<div id="main">
 			<h1>Album Creator</h1>
@@ -39,9 +39,6 @@
 			<div id="button" height="80px" width="150px" >Create Your Photo-Album</div>
 			<?php include("Footer.php"); ?>
 		</div>
-		<script>
-		  FB.login(function(response){});
-		</script>
 	</body>
 
 </html>
