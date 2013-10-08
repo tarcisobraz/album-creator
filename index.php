0574135e8646e3843eabc4e68d7069a46e82f20f
Tarciso Braz <tarcisocomp@gmail.com>
2013-10-08 10:23:35 -0300

    Moving js files to script.js

--- index.php
+++ index.php
@@ -51,37 +51,6 @@
 		</div>
 		
 		<script>
-			function loginFB() {
-				FB.login(function(response) {
-					if (response.authResponse) {
-						sayHello();
-// 						window.open("AlbumChoice.php", "_self");
-					}				
-				}, {scope: 'email,user_photos,publish_actions'});
-			};
-
-			function sayHello() {
-				console.log('Welcome! Fetching your information...');
-				FB.api('/me', function(response) {
-					console.log('Good to see you ' + response.name);
-				});
-				getAlbums();
-			};
-
-			function getAlbums() {
-				console.log('Fetching albums info...');
-				var albumsResponse = FB.api('/me/albums?fields=id,name', function(response) {
-					var albums = response.data;
-				for (var i = 0; i < albums.length; i++) {
-					console.log('Album ' + i + ': ' + albums[i].name);
-				}
-					return albums;
-				});
-
-				return albumsResponse;
-				
-			};
-
 			function createAlbumsTable() {
 				console.log('Creating Albums table');
 				var myAlbums = getAlbums();
--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function getAlbums() {
-	console.log('Fetching albums info...');
-	var albumsResponse = FB.api('/me/albums?fields=id,name', function(response) {
-		var albums = response.data;
-//	for (var i = 0; i < albums.length; i++) {
-//		console.log('Album ' + i + ': ' + albums[i].name);
-//	}
-		return albums;
-	});
-
-	return albumsResponse;
-	
+function loginFB() {
+	FB.login(function(response) {
+		if (response.authResponse) {
+			sayHello();
+// 						window.open("AlbumChoice.php", "_self");
+		}				
+	}, {scope: 'email,user_photos,publish_actions'});
 };
 
 function sayHello() {
@@ -17,31 +12,19 @@ function sayHello() {
 	FB.api('/me', function(response) {
 		console.log('Good to see you ' + response.name);
 	});
-//	getAlbums();
+	getAlbums();
 };
 
-function createAlbumsTable() {
-	console.log('Creating Albums table');
-	var myAlbums = getAlbums();
-
-	var albumsTHeader = "<thead><tr><th>Choose the album to be used as source</th></tr></thead>";
-	var albumsTBody = "<tbody>";
-
-	for (var i = 0; i < myAlbums.length; i++) {
-		console.log('Inserting album ' + myAlbums[i].name + ' in the table.');
-		var albumCoverImg = FB.api('/' + myAlbums[i].id + '/photos?fields=source', function(response) {
-			return response.data[0].source;
-		});
-		var albumRow = "<tr style='text-align: center;'><td><img height=\"80px\" width=\"100px\" src=" + albumCoverImg + "> " + myAlbums[i].name + "</td></tr>";
-		console.log(albumRow);
-		albumsTBody += albumRow;
+function getAlbums() {
+	console.log('Fetching albums info...');
+	var albumsResponse = FB.api('/me/albums?fields=id,name', function(response) {
+		var albums = response.data;
+	for (var i = 0; i < albums.length; i++) {
+		console.log('Album ' + i + ': ' + albums[i].name);
 	}
+		return albums;
+	});
 
-	console.log("Finished inserting albums to the table. Loading table now!");
+	return albumsResponse;
 	
-	albumsTBody += "</tbody>";
-
-	var myAlbumsTable = "<table border=1px>" + albumsTHeader + albumsTBody + "</table>";
-	
-	document.getElementById('albumsTable').innerHTML = myAlbumsTable;
 };
